266b94e91c8501eee7dccc0fc350cc320bec8496
RWMumo <ritawanza5@gmail.com>
2022-07-22 11:20:53 +0300

    galleryfolder

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<username>:Kikanyi1776.@clusterweek4ip.zd7gs.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<username>:Kikanyi1776.@clusterweek4ip.zd7gs.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<username>:Kikanyi1776.@clusterweek4ip.zd7gs.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://rwmumo1:zOfiexZXOmqs8CB3@clusterweek4ip.zd7gs.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://rwmumo1:zOfiexZXOmqs8CB3@clusterweek4ip.zd7gs.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://rwmumo1:zOfiexZXOmqs8CB3.@clusterweek4ip.zd7gs.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
